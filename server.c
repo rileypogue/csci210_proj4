a8eaccffe34f8c2b1835787cf10e1cd124998ce7
Riley Pogue <pogue@mines.edu>
2024-12-04 13:07:55 -0700

    three?

--- server.c
+++ server.c
@@ -33,7 +33,7 @@ int main() {
 		// read requests from serverFIFO
 		read(server, &req, sizeof(struct message));
 
-		// printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
+		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
 
 		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
